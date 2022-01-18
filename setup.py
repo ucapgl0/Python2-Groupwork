6f0a33f5812ea7596661ff7f1f24dcce90d328bc
felixFelicisss <1158707946@qq.com>
2022-01-18 01:17:12 +0000

    fix the path of entry points

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name = "tracknaliser",
     version = "0.1.0",
     packages = find_packages(exclude=['*.test']),
-    install_requires = ['pytest','requests','matpltlib','numpy','numpydoc'],
-    entry_points = {'console_scripts':['greentrack = tracknaliser-Working-Group-20.command:process']}
+    install_requires = ['pytest','requests','matplotlib','numpy','numpydoc'],
+    entry_points = {'console_scripts':['greentrack = command:process']}
 )
