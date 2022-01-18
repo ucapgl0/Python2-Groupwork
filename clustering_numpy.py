c321dcef773d8b8f302039bf717f0cd97c620f1a
Dom98 <dominic.kufel@gmail.com>
2022-01-18 01:26:10 +0000

    Optimising the numpy clustering code

--- clustering_numpy.py
+++ clustering_numpy.py
@@ -1,62 +1,47 @@
-import numpy as np 
 import random
+import numpy as np
 
 # Checklist of proposed changes:
 
 # 1. import math, random, not as * [x] e0c59b01f9daaa88ce26ebf139c15882ef1c87e5
 # 2. Comment explaining what the code does [x] c2841c373e0e919c6061e2ad943ec498e2c669c6
 # 3. Rename some of the variables (e.g. ps) [x] e5d2dbd79b0a5115d95aca0567e4da935a3dd891
-# 4. Use numpy where possible to speedup the code [ ]
-# 5. Define the cluster function [x]
-# 6. Improving readability by explicitly showing dependence on k. [x]
+# 4. Use numpy where possible to speedup the code [x]
+# 5. Define the cluster function [x] 94b51d498c123896b9e6dce3f68c49b9148d7cc0
+# 6. Improving readability by explicitly showing dependence on k. [x] 94b51d498c123896b9e6dce3f68c49b9148d7cc0
 
-k=3 #The number of clusters of nearby points 
 
 #Reading the file containing points in 3D space
 lines = open('samples.csv', 'r').readlines() #read file line by line
-parray=np.zeros(len(lines)) # TODo change this to a numpy array point list: list containing each point coordinates (written as a tuple)
+plist=[] #point list: list containing each point coordinates (written as a tuple)
 for line in lines: #iterate through the line and add each point coordinates
-  parray[line] = (tuple(map(float, line.strip().split(',')))) #TODo Change this to an array indexing 
+  plist.append(list(map(float, line.strip().split(',')))) 
 
-def cluster(parray,n=10):
-    #Pick k points at random for the initial cluster centres
-    m=[parray[random.randrange(parray.shape[0])] for it in range(0,k)] #TODO change this from list comprehension to array indexing 
-    #m=[parray[random.randrange(len(parray))], parray[random.randrange(len(parray))], parray[random.randrange(len(parray))]]    
+parray=np.array(plist) #create a numpy array from the data
 
-    alloc=[None]*parray.shape[0]  # TODo change this to parray.shape[0] list which contains allocates point to one of the three clusters
-    n=0
-    while n<10: #iterate the k-means algorithm
-        for i in range(len(parray)):  #iterate through the list of all points 
-            p=parray[i] #index of the point in the point list
-            d=[None] * k #list of distances of a point to each of the cluster centres
-            for q in range(0,k):
-                d[q]=np.sqrt((p[0]-m[q][0])**2 + (p[1]-m[q][1])**2 + (p[2]-m[q][2])**2) #distance of the point to the q-th cluster
-            alloc[i]=d.index(min(d)) #assign the point to the cluster which is the closest to it
-
-        for i in range(k):  #loop through for each of the k clusters
-            alloc_parray=[p for j, p in np.ndenumerate(parray) if alloc[j] == i] # TODo change this from enumerate make a list of all points within a given cluster
-            #within each cluster find the (virtual) point by averaging over all cluster points
-            new_mean=(sum([a[0] for a in alloc_parray]) / len(alloc_parray), sum([a[1] for a in alloc_parray]) / len(alloc_parray), sum([a[2] for a in alloc_parray]) / len(alloc_parray))
-            #use such (virtual) point to be a new cluster centre
-            m[i]=new_mean
-        n=n+1 #repeat the above procedure 10 times
+def cluster(plist,n=10,k=3):
+    m=np.random.randint(0,np.shape(parray)[0],size=k)
+    for c in range(0,n):
+        alloc=np.argmin(np.array([np.sqrt(np.sum((parray-m[j])**2,axis=1)) for j in range(0,k)]),axis=0)
+        m=np.array([np.mean(parray[alloc==j],axis=0) if np.all(np.isnan(np.mean(parray[alloc==j],axis=0)))==False else np.array([0,0,0]) for j in range(0,k)])
     return alloc,m
 
-alloc,m=cluster(parray) #TODO change these indexes
+k=3 #The number of clusters of nearby points 
+alloc,m=cluster(plist,10,k)
 # print(alloc)
 
-#### OUTPUTING THE ALGORITHM RESULTS 
+##### OUTPUTING THE ALGORITHM RESULTS 
 # # Text output
 # for i in range(k):
-#   alloc_parray=[p for j, p in enumerate(parray) if alloc[j] == i]
-#   print("Cluster " + str(i) + " is centred at " + str(m[i]) + " and has " + str(len(alloc_parray)) + " points.")
-#   print(alloc_parray)
+#   alloc_plist=[p for j, p in enumerate(plist) if alloc[j] == i]
+#   print("Cluster " + str(i) + " is centred at " + str(m[i]) + " and has " + str(len(alloc_plist)) + " points.")
+#   print(alloc_plist)
 
-#Visual output
-from matplotlib import pyplot as plt 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-for i in range(k):
-  alloc_parray = [p for j, p in np.ndenumerate(parray) if alloc[j]==i]
-  ax.scatter([a[0] for a in alloc_parray],[a[1] for a in alloc_parray],[a[2] for a in alloc_parray])
-plt.show()
+# #Visual output
+# from matplotlib import pyplot as plt 
+# fig = plt.figure()
+# ax = fig.add_subplot(projection='3d')
+# for i in range(k):
+#   alloc_plist = [p for j, p in enumerate(plist) if alloc[j]==i]
+#   ax.scatter([a[0] for a in alloc_plist],[a[1] for a in alloc_plist],[a[2] for a in alloc_plist])
+# plt.show()
