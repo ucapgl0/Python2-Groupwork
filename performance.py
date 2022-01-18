ad8ab0380a86e5edb2ca670d3a2259876d78699e
SolemnShark871 <46021567+SolemnShark871@users.noreply.github.com>
2022-01-18 11:57:28 +0000

    #13 Generate the paths

--- performance.py
+++ performance.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib as plt
 from tracks import SingleTrack # TODO this may need to be fixed 
+from clustering import cluster
+from clustering_numpy import cluster 
 
 # Run both versions of the clustering scripts 
 # For input files with a given number of points ranging from 100 - 10,000
@@ -16,13 +18,19 @@ for N in N_points:      #Loop through all the number of points
 
         # Generate N-1 random points (x,y) from the sample of points (integers in range 300)  
 
-        chain_code =  zip(np.round(np.random.rand(N-1)*300), np.round(np.random.rand(N-1)*300))
+        chain_code =  str(zip(np.round(np.random.rand(N-1)*300), np.round(np.random.rand(N-1)*300)))
 
+        elevation = str(np.random.rand(N-1)) #randomly assorted elevations of points in [0,1] range
             #TODO check that this works 
+        #Generate the path as a SingleTrack object
+
+        path = SingleTrack((1,1),(299,299),1,chain_code,r,p,elevation)
+
 
 
 
     # Run with clustering.py & benchmark 
+
 
 
     # Run with clustering_numpy.py & benchmark 
