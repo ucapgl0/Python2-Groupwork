541270238e630d72d7a9ca0863e736c52c6cb7fd
SolemnShark871 <46021567+SolemnShark871@users.noreply.github.com>
2022-01-18 22:37:59 +0000

    #13 Added timining functionality

--- performance.py
+++ performance.py
@@ -1,42 +1,47 @@
 import numpy as np
 import matplotlib as plt
+from time import time 
 from tracks import SingleTrack # TODO this may need to be fixed 
-from clustering import cluster
-from clustering_numpy import cluster 
+from clustering import cluster as cluster_list
+from clustering_numpy import cluster as cluster_numpy 
 
 # Run both versions of the clustering scripts 
 # For input files with a given number of points ranging from 100 - 10,000
 # Plot time against required input with a single plot for both
 # Save as performance.png 
 
-N_points = np.logspace(100,10000,5) #Define an array of the number of points 
+num_points = 5
 
-# Generate the input files with the nmber of points
+N_points = np.logspace(100,10000,num_points) #Define an array of the number of points 
+times = np.zeros((num_points,2)) #array to store the times in 
 
-for N in N_points:      #Loop through all the number of points
-    #Generate the input files with a given number of files
+i = 0 
+for N in N_points:      #Loop through all the number of points in the range 
+        #Generate the input files with a given number of points
 
         # Generate N-1 random points (x,y) from the sample of points (integers in range 300)  
 
         chain_code =  str(zip(np.round(np.random.rand(N-1)*300), np.round(np.random.rand(N-1)*300)))
-
         elevation = str(np.random.rand(N-1)) #randomly assorted elevations of points in [0,1] range
             #TODO check that this works 
         #Generate the path as a SingleTrack object
-
         path = SingleTrack((1,1),(299,299),1,chain_code,r,p,elevation)
 
 
+        # Run with clustering.py & benchmark 
 
+        start_list =time()
+        cluster_l = cluster_list(path)
+        end_list = time()
 
-    # Run with clustering.py & benchmark 
-
-
-
-    # Run with clustering_numpy.py & benchmark 
-
-
+        times[i,0] = end_list - start_list #benchmark time
+        # Run with clustering_numpy.py & benchmark 
+        start_numpy =time()
+        cluster_np = cluster_numpy(path)
+        end_numpy =time()
+        times[i,1] = end_numpy - start_numpy
 # Plot both times as a single plot
+
 
 
 # Save the plot 
