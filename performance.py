995d87bdefdce0e152e17399a1a044ac04661871
SolemnShark871 <46021567+SolemnShark871@users.noreply.github.com>
2022-01-18 11:46:40 +0000

    Measuring Performance  #13 generated chain code for track

--- performance.py
+++ performance.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib as plt
+from tracks import SingleTrack # TODO this may need to be fixed 
 
 # Run both versions of the clustering scripts 
 # For input files with a given number of points ranging from 100 - 10,000
@@ -10,10 +11,21 @@ N_points = np.logspace(100,10000,5) #Define an array of the number of points
 
 # Generate the input files with the nmber of points
 
-# Run with clustering.py & benchmark 
+for N in N_points:      #Loop through all the number of points
+    #Generate the input files with a given number of files
+
+        # Generate N-1 random points (x,y) from the sample of points (integers in range 300)  
+
+        chain_code =  zip(np.round(np.random.rand(N-1)*300), np.round(np.random.rand(N-1)*300))
+
+            #TODO check that this works 
 
 
-# Run with clustering_numpy.py & benchmark 
+
+    # Run with clustering.py & benchmark 
+
+
+    # Run with clustering_numpy.py & benchmark 
 
 
 # Plot both times as a single plot
