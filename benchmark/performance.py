febb5d6d31f5a15ef385486e4b88c03d9592a7fb
ucapgl0 <91620426+ucapgl0@users.noreply.github.com>
2022-01-26 21:36:36 +0000

    modify some comments

--- benchmark/performance.py
+++ benchmark/performance.py
@@ -1,50 +1,33 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from time import time
-
-# import subprocess
-
-# final_path=str(Path.cwd().parent)
-# print('Final Path')
-# print(final_path)       #TODO remove this when done
-# subprocess.run('python'+str(final_path)+'/tracknaliser/clustering.py'+' samples.csv', shell=True)
-
-# #import functions from tracknaliser 
-
+# #import functions from tracknaliser
 import sys
 sys.path.append('../tracknaliser')
-
 from tracknaliser.clustering import cluster as cluster_list
-from tracknaliser.clustering_numpy import cluster as cluster_numpy 
-
-# Run both versions of the clustering scripts 
-# For input files with a given number of points ranging from 100 - 10,000
+from tracknaliser.clustering_numpy import cluster as cluster_numpy
+# Run both versions of the clustering scripts
 # Plot time against required input with a single plot for both
-# Save as performance.png 
-
+# Save as performance.png
 N_points = np.arange(100, 10001, 100)
 np.random.seed(0)
 test_point = np.random.random_sample((10000,3)) #Define an array of the number of points 
 times = np.zeros((2,len(N_points)), dtype=float) #array to store the times in 
 
-i = 0 
-for i in range(len(N_points)):      #Loop through all the number of points in the range 
-        #Generate the input files with a given number of points
-
-        # Generate N-1 random points (x,y) from the sample of points (integers in range 300) 
-         
+for i in range(len(N_points)):
         # Run with clustering.py & benchmark 
         start_list =time()
         cluster_l = cluster_list(test_point[0:100*(i+1)])
         end_list = time()
-        times[0][i] = end_list - start_list #benchmark time
+        times[0][i] = end_list - start_list
+
         # Run with clustering_numpy.py & benchmark 
         start_numpy =time()
         cluster_np = cluster_numpy(test_point[0:100*(i+1)])
         end_numpy =time()
         times[1][i] = end_numpy - start_numpy
-# Plot both times as a single plot
 
+# Plot both times as a single plot
 plt.plot(N_points, times[0], label = "Native python method")
 plt.plot(N_points, times[1], label = "Numpy method")
 plt.title("Time taken for various input path lengths")
